19762f9c6f29c0379ce54186e134bb69feddd732
John Ayad <johnhany97@gmail.com>
2018-05-26 00:14:36 +0100

    Async HeroService using Observable

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -4,7 +4,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
 
 @Component({ // Following is automatically generated three metadata properties
   // Selector css element also matches name of the HTML element that identifies this component
@@ -18,19 +18,25 @@ export class HeroesComponent implements OnInit {
     id: 1,
     name: 'Windstorm'
   };
-  heroes = HEROES;
+  heroes: Hero[];
   selectedHero: Hero;
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
   }
 
-  constructor() { }
+  constructor(private heroService: HeroService) { }
 
   // Lifecycle hook
   // Angular calls this shortly after creating a component
   // Where you usually initialise
   ngOnInit() {
+    this.getHeroes();
+  }
+
+  getHeroes(): void {
+    this.heroService.getHeroes()
+      .subscribe(heroes => this.heroes = heroes);
   }
 
 }
